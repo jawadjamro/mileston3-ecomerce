73326207b606e5789b8a9e2bd58be8e8535ffaa8
Jawad Hassan <159148373+jawadjamro@users.noreply.github.com>
2025-01-21 05:09:48 -0800

    Complete

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,7 +1,6 @@
 import Link from "next/link";
 import React from "react";
 import { twMerge } from "tailwind-merge";
-import { SiNike } from "react-icons/si";
 
 
 const Logo = ({ className }: { className?: string }) => {
